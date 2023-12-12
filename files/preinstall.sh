a4651313f3f119c6166e177247d6fd55dea84fac
James-Livesey <jamesliveg@gmail.com>
2023-12-12 20:19:41 +0000

    Add WIP release for graphical Linux app integration

---
+++ files/preinstall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# gShell
+# 
+# Copyright (C) LiveG. All Rights Reserved.
+# 
+# https://liveg.tech
+# Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
+
+pushd /system/storage/update
+
+tee /system/logs/update-preinstall.log << EOF
+PLATFORM=$PLATFORM
+MODEL_CODENAME=$MODEL_CODENAME
+VERNUM=$VERNUM
+VERSION=$VERSION
+OLD_VERNUM=$OLD_VERNUM
+OLD_VERSION=$OLD_VERSION
+EOF
+
+mkdir -p /usr/share/gtk-3.0
+
+if [ "$OLD_VERNUM" -le 4 ]; then
+    sudo apt install -y fonts-urw-base35=20200910-7
+
+    sudo rm -rf /usr/share/themes/Adapt-UI-Linux
+    sudo mkdir -p /usr/share/themes/Adapt-UI-Linux
+    sudo tar -xzf Adapt-UI-Linux.tar.gz -C /usr/share/themes/Adapt-UI-Linux
+fi
+
+popd
