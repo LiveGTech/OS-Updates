d0e6e1c53954d13c7d9ccb9aa3c1f9f3b55e5205
James-Livesey <jamesliveg@gmail.com>
2024-11-03 20:25:33 +0000

    Create a test release for V0.3.0

--- files/postinstall.sh
+++ files/postinstall.sh
@@ -13,4 +13,15 @@ sed -i "s|startx /system/scripts/xload.sh > /dev/null 2>&1|startx /system/script
 
 chmod +x /system/scripts/xload.sh
 
+# Fixes https://github.com/LiveGTech/OS-Updates/issues/1
+chmod -R o-w /system
+
+find /system \
+    -not -path "/system/control/*" \
+    -and -not -path "/system/storage/users/*/files/*" \
+    -and -not -path "/system/.cache/*" \
+    -and -not -path "/system/.config/gshell/Partitions/*" \
+    -and -not -path "/system/.config/gshell/Cache/*" \
+    -exec chmod o-w {} \;
+
 popd
